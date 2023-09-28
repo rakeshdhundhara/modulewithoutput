1b0e4f8f455b668bfe371d2bee0f8e36b370944f
rakeshdhundhara <143716231+rakeshdhundhara@users.noreply.github.com>
2023-09-28 23:21:34 +0530

    change

---
+++ variable.tf
@@ -0,0 +1,29 @@
+variable "inst" {
+  type = map(any)
+}
+# variable "tags" {
+#   type = map()
+# }
+variable "Availability" {
+
+ type        = list(string)
+
+ description = "Availability Zones"
+
+#  default     = ["ap-south-1a", "ap-south-1b"]
+}
+
+
+
+ 
+
+variable "private_subnet_cidr" {
+
+ type        = list(string)
+
+ description = "Private Subnet CIDR values"
+
+}
+variable "tags" {
+  type = map(any)
+}
